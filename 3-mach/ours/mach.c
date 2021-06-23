707c8a4ab31018dfff5d2e16db1a4d2dbfd7b912
broecrd9 <rouven.broeckelmann@ruhr-uni-bochum.de>
2021-06-23 13:39:54 +0200

    Read file and synchronous execute

--- 3-mach/ours/mach.c
+++ 3-mach/ours/mach.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include "run.h"
 
-#define MAX_THREADS 5
-
-/*static void die(const char *s) {
+#define MAX_LINE 4096
+static void die(const char *s) {
     perror(s);
     exit(EXIT_FAILURE);
 }
+
 
 static int parse_positive_int_or_die(char *str) {
     errno = 0;
@@ -31,9 +32,30 @@ static int parse_positive_int_or_die(char *str) {
         exit(EXIT_FAILURE);
     }
     return (int)x;
-}*/
+}
 
 int main(int argc, char **argv) {
-    // TODO: implement me
-    printf("Test\n");
+    if(argc != 3)
+    {
+		fprintf(stderr, "usage: ./mach <anzahl threads> <mach-datei>\n");
+		return EXIT_FAILURE;
+	}
+	
+	int max_threads = parse_positive_int_or_die(argv[1]);
+	
+	char* path_to_file = argv[2];
+	char file_line[MAX_LINE + 1];
+	FILE* file = fopen(path_to_file, "r");
+	if(!file)
+		die("File");
+	
+	while (fgets(file_line, MAX_LINE, file) != NULL) {
+		printf(file_line);
+		char* cmd_out = NULL;
+		run_cmd(file_line, &cmd_out);
+		printf("%s\n", cmd_out);
+	}
+		
+    printf("Test %d\n", max_threads);
+    return EXIT_SUCCESS;
 }
